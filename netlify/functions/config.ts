f5bb20df2bcdabe3b8013cfbaa122793e270711b
ryna6 <ryanjw629.h@gmail.com>
2026-02-05 08:37:54 -0500

    Update config.ts

--- netlify/functions/config.ts
+++ netlify/functions/config.ts
@@ -1,1 +1,13 @@
+import type { Context } from "@netlify/functions";
+import { TICKERS } from "../../shared/tickers";
+
+export default async (_req: Request, _context: Context) => {
+  return new Response(JSON.stringify({ tickers: TICKERS }), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "public, max-age=60"
+    }
+  });
+};
 
